a77607668cec6b07e67b1bdc44f835b78c9d61ae
Kaan <119818534+kaan1212@users.noreply.github.com>
2024-11-12 10:13:11 +0100

    Add generated expr.py file.

---
+++ lox/expr.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+
+class Expr(ABC):
+    @abstractmethod
+    def accept(self, visitor): pass
+
+
+class Visitor(ABC):
+    @abstractmethod
+    def visitbinaryexpr(self, expr): pass
+    @abstractmethod
+    def visitgroupingexpr(self, expr): pass
+    @abstractmethod
+    def visitliteralexpr(self, expr): pass
+    @abstractmethod
+    def visitunaryexpr(self, expr): pass
+
+
+class Binary(Expr):
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor):
+        return visitor.visitbinaryexpr(self)
+
+
+class Grouping(Expr):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor):
+        return visitor.visitgroupingexpr(self)
+
+
+class Literal(Expr):
+    def __init__(self, value):
+        self.value = value
+
+    def accept(self, visitor):
+        return visitor.visitliteralexpr(self)
+
+
+class Unary(Expr):
+    def __init__(self, operator, right):
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor):
+        return visitor.visitunaryexpr(self)
